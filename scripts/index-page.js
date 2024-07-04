3d99487bca33b7a4f088d3fc4cd9792205a2026f
idaoskooei <idaoskooei@gmail.com>
2024-07-04 13:36:47 -0400

    basic design

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,11 +1,120 @@
-const url = "https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={4ef30b9089e6aebb1f6c0702cbbd1b18}" ; 
-console.log(url);
+// const url = "https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={4ef30b9089e6aebb1f6c0702cbbd1b18}" ; 
+// console.log(url);
 
-async function fetchWeather(){
-    const response = await axios.get(url);
-    return response;
-}
+// async function fetchWeather(){
+//     const response = await axios.get(url);
+//     return response;
+// }
 
-let response = fetchweather();
-console.log(response.data);
-console.log(response.data.data);
+// let response = fetchweather();
+// console.log(response.data);
+// console.log(response.data.data);
+
+
+
+var cityInput = document.getElementById("searchCity");
+
+var backgroundsList = [
+  "day1.jpg",
+  "day2.jpg",
+  "day3.jpg",
+  "day4.jpg",
+  "day5.jpg",
+  "night1.jpg",
+  "night2.jpg",
+  "night3.jpg",
+  "night4.jpg",
+  "night5.jpg",
+  "cloudy1.jpg",
+  "cloudy2.jpg",
+  "cloudy3.jpg",
+  "cloudy4.jpg",
+  "cloudy5.jpg",
+  "rainy1.jpg",
+  "rainy2.jpg",
+  "rainy3.jpg",
+  "rainy4.jpg",
+  "rainy5.jpg",
+];
+
+var randomBackground = backgroundsList[Math.floor(Math.random() * backgroundsList.length)];
+
+document.body.style.background = "linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)) , url('media/" + randomBackground + "')";
+
+cityInput.addEventListener("keyup", function(event)
+{
+  if(event.key === "Enter")
+  {
+		loader();
+		function loader()
+		{
+
+			document.getElementById("locationName").innerHTML = "";
+			document.getElementById("temperatureValue").innerHTML = "";
+			document.getElementById("weatherType").innerHTML = "";
+
+			const img1 = document.createElement("img");
+			const img2 = document.createElement("img");
+			const img3 = document.createElement("img");
+
+			img1.id = "loader1";
+			img2.id = "loader2";
+			img3.id = "loader3";
+
+			img1.src = "icons/loader.gif";
+			img2.src = "icons/loader.gif";
+			img3.src = "icons/loader.gif";
+
+			const parentElement1 = document.getElementById("locationName");
+			const parentElement2 = document.getElementById("temperatureValue");
+			const parentElement3 = document.getElementById("weatherType");
+
+			parentElement1.appendChild(img1);
+			parentElement2.appendChild(img2);
+			parentElement3.appendChild(img3);
+		}
+
+    var cityInputValue = cityInput.value;
+
+    var apiKey = "4ef30b9089e6aebb1f6c0702cbbd1b18";
+    var unit = "metric";
+    var url = `https://api.openweathermap.org/data/2.5/weather?q=${cityInputValue}&appid=${apiKey}&units=${unit}`;
+
+    if(cityInputValue != "")
+    {
+      async function getWeather()
+      {
+        var response = await fetch(url);
+        var data = await response.json();
+
+        if(data.message != "city not found" && data.cod != "404")
+        {
+          var location = data.name;
+          var temperature = data.main.temp;
+          var realFeel = data.main.feels_like;
+          var maxTemperature = data.main.temp_max;
+          var minTemperature = data.main.temp_min;
+          var humidity = data.main.humidity;
+        
+        
+          document.getElementById("locationName").innerHTML = location;
+          document.getElementById("temperatureValue").innerHTML = temperature + "<sup>o</sup>C";
+          document.getElementById("realFeelAdditionalValue").innerHTML = realFeel + "<sup>o</sup>C";
+          document.getElementById("visibilityAdditionalValue").innerHTML = visibility + " km";
+          document.getElementById("maxTemperatureAdditionalValue").innerHTML = maxTemperature + "<sup>o</sup>C";
+          document.getElementById("minTemperatureAdditionalValue").innerHTML = minTemperature + "<sup>o</sup>C";
+          document.getElementById("humidityAdditionalValue").innerHTML = humidity;
+        }
+        else
+				{
+					document.getElementById("locationName").innerHTML = "City Not Found";
+					document.getElementById("temperatureValue").innerHTML = "";
+					document.getElementById("weatherType").innerHTML = "";
+				}
+      }
+
+      getWeather();
+    }
+    else document.getElementById("locationName").innerHTML = "Enter a city name...";
+  }
+});
